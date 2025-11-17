d2ead1fd0d94a0a626b9858e5657ecfc113b5792
Niraj Nillawar <149431848+NirajDN@users.noreply.github.com>
2025-11-17 23:11:36 +0530

    Time: 15 ms (10.16%), Space: 23.9 MB (12.38%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& arr) {
+        int n = arr.size(); 
+        unordered_map<int, int> mp; 
+        for(int i = 0; i < n; i++)
+        {
+            mp[arr[i]]++;
+        }
+        int ans; 
+        for(auto x: mp) 
+        {
+            if(x.second == 1) 
+            {
+                ans = x.first; 
+                break; 
+            }
+        }
+        return ans; 
+    }
+};
