963eed077038cca5e5d9c3a403641ae38ffbd070
Niraj Nillawar <149431848+NirajDN@users.noreply.github.com>
2026-01-24 09:48:12 +0530

    Time: 0 ms (100%), Space: 10.3 MB (85.87%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        int n=piles.size();
+        int maxpil=0;
+        int esum=0;
+        int odsum=0;
+        // for(int i=0;i<n;i+2){
+        //     odsum=odsum+piles[i];
+        // }
+        // for(int i=1;i<n;i+2){
+        //     esum=esum+piles[i];
+        // }
+        return true;
+    }
+};
