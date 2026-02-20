c7bb3e44c6168e1d3fa73e5008b1481098db4fd9
Niraj Nillawar <149431848+NirajDN@users.noreply.github.com>
2026-02-20 14:38:39 +0530

    Time: 2 ms (14.9%), Space: 11 MB (47.37%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        unordered_set<int>set;
+        for(auto v: nums)if(v!=0)set.insert(v);
+        return set.size();
+    }
+};
