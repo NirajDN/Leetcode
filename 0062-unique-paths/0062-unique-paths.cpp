3089bb66762dfabaeb198f561dba92ebf7767898
Niraj Nillawar <149431848+NirajDN@users.noreply.github.com>
2026-06-25 10:37:01 +0530

    Time: 0 ms (100%), Space: 9.5 MB (29.21%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int totalmaze(int i, int j, int m, int n, vector<vector<int>>& dp) {
+        if (i == m - 1 && j == n - 1) return 1;
+
+        if (i >= m || j >= n) return 0;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int down = totalmaze(i + 1, j, m, n, dp);
+        int right = totalmaze(i, j + 1, m, n, dp);
+
+        return dp[i][j] = down + right;
+    }
+
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return totalmaze(0, 0, m, n, dp);
+    }
+};
