401ac94e74a165623da74d6cc7bf05b9eb3429c2
Niraj Nillawar <149431848+NirajDN@users.noreply.github.com>
2026-06-26 11:17:28 +0530

    Time: 0 ms (100%), Space: 13.5 MB (24.67%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int> > &triangle) 
-    {
-        vector<int> mini = triangle[triangle.size()-1];
-        for ( int i = triangle.size() - 2; i>= 0 ; --i )
-            for ( int j = 0; j < triangle[i].size() ; ++ j )
-                mini[j] = triangle[i][j] + min(mini[j],mini[j+1]);
-        return mini[0];
+    int solve(int i, int j, vector<vector<int>>& triangle, int n
+    ,vector<vector<int>>&dp){
+        if(i==n-1){
+            return triangle[i][j];
+        }
+        if(dp[i][j]!=INT_MAX){
+            return dp[i][j];
+        }
+
+        int down=triangle[i][j]+solve(i+1,j,triangle,n,dp);
+        int diag=triangle[i][j]+solve(i+1,j+1,triangle,n,dp);
+        return dp[i][j]=min(down,diag);
+
+
+
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        vector<vector<int>>dp(n,vector<int>(n,INT_MAX));
+        return solve(0,0,triangle,n,dp); 
     }
 };
