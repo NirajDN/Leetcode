f1a341d928a29a3ebd681876760ceab8121a2a00
Niraj Nillawar <149431848+NirajDN@users.noreply.github.com>
2026-06-29 17:04:44 +0530

    Time: 0 ms (100%), Space: 7.8 MB (49.38%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
+    bool get(int n){
+        if(n <= 0) return false;
+        if(n == 1) return true;
+        if(n % 2 != 0) return false;
+
+        return get(n / 2);
+    }
+
     bool isPowerOfTwo(int n) {
-        return n > 0 && not (n & n - 1);
+        return get(n);
     }
 };
