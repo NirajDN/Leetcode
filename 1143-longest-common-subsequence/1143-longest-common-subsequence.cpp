4a06d3e6d1add82c0fbe818b29db29f015ba8e64
Niraj Nillawar <149431848+NirajDN@users.noreply.github.com>
2026-06-29 11:13:23 +0530

    Time: 31 ms (36.86%), Space: 27.5 MB (30.21%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -8,9 +8,6 @@ public:
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(i<0 || j<0){
-                    return 0;
-                }
                 if(text1[i]==text2[j]){
                     int a=0;
                     if(i>0 && j>0){
