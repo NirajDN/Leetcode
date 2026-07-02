4b556772d781155ad75fbbda1f84fd2f040dca60
Niraj Nillawar <149431848+NirajDN@users.noreply.github.com>
2026-07-02 14:44:09 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    int climb(int i,int n,vector<int>&dp){
+    int solve(int i,int n,vector<int> & dp){
         if(i==n){
             return 1;
         }
-        if(i>=n){
+        if(i>n){
             return 0;
         }
         if(dp[i]!=-1){
             return dp[i];
         }
-
-        int oj=climb(i+1,n,dp);
-        int tj=climb(i+2,n,dp);
-        return dp[i]=oj+tj;
-
+        int oc=solve(i+1,n,dp);
+        int tc=solve(i+2,n,dp);
+        return dp[i]=oc+tc;
     }
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
-        return climb(0,n,dp);
+        return solve(0,n,dp);
         
     }
 };
