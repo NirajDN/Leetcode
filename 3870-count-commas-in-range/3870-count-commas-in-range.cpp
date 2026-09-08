b7b30345f6511cb8f0422436ff9f6443bb6a47db
Niraj Nillawar <149431848+NirajDN@users.noreply.github.com>
2026-09-08 14:51:36 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.38%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int count=0;
+        while(n>999){
+            count++;
+            n--;
+        }
+       return count; 
+    }
+};
